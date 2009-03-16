239333c68f6fda8d3d0a37bb70846bbf16b4b3a2
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-03-16 23:21:52 +0000

    Protocol test scripts

--- tests/testMultiBoxNtcpLink.pl
+++ tests/testMultiBoxNtcpLink.pl
@@ -1,10 +1,11 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl -w
 
 use IO::Socket::INET;
 use File::Spec;
 use Cwd;
+use strict;
 
-my ($host, $port) = ("cee-zzul.cee.uiuc.edu","6342");
+my ($host, $port) = ("127.0.0.1",6342);
 #my ($host, $port) = ("192.168.1.101","5057");
 my $cwd =cwd();
 my @dirs = File::Spec->splitdir($cwd);
@@ -12,48 +13,57 @@ my $dropped = pop @dirs;
 my $pwd = File::Spec->catdir(@dirs);
 
 
-my $socket = new IO::Socket::INET(
-		PeerAddr => $host,
-		PeerPort => $port,
-		Proto     => 'tcp',
-	);
+my $lsocket = new IO::Socket::INET(
+                                   LocalAddr => $host,
+                                   LocalPort => $port,
+                                   Proto     => "tcp",
+                                   Listen => 5,
+                                   Reuse => 1.
+                                  );
 
-die "Connection to $host:$port failed because $!" unless defined $socket;
+die "Connection to $host:$port failed because $!" unless defined $lsocket;
 
-sendCommand("open-session dummySession");
-receiveCommand();
+my $running = 1;
+my $socket = $lsocket->accept();
 
-sendCommand("set-parameter dummySetParam	nstep	0");
-receiveCommand();
-
-my $increment = "0.5";
-for my $i (1..500) {
-	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
-	$year += 1900;
-	$month++;
-	print "$mon/$mday/$year";
-	sendCommand("propose\ttrans$year$month$mday$hour$min$sec.320\tMDL-00-01:LBCB 1\tx\tdisplacement\t$increment\ty\tdisplacement\t0.0");
-	receiveCommand();
-	sendCommand("propose\ttrans$year$month$mday$hour$min$sec.320\tMDL-00-01:LBCB 2\tx\tdisplacement\t$increment\ty\tdisplacement\t0.0");
-	receiveCommand();
-	sendCommand("execute\ttrans$year$month$mday$hour$min$sec.320");
-	receiveCommand();
-	sendCommand("get-control-point\tdummy\tMDL-00-01:LBCB 1");
-	receiveCommand();
-	sendCommand("get-control-point\tdummy\tMDL-00-01:LBCB 2");
-	receiveCommand();
-	sendCommand("get-control-point\tdummy\tMDL-00-01:External Sensors");
-	receiveCommand();
-	$increment = $increment eq "0.5" ? "-0.5" : "0.5";
-	sleep 3;
+while ($running) {
+  my($keyword, $trans, $mdl) = receiveCommand();
+  if ($keyword eq 'open-session') {
+    sendResponse("OK 0\tOpen Session Succeeded");
+    next;
+  }
+  if ($keyword eq 'set-parameter') {
+    sendResponse("OK 0\tCommand ignored. Carry on.");
+    next;
+  }
+  if ($keyword eq 'propose') {
+    sendResponse("OK\t0\t$trans\tpropose accepted");
+    next;
+  }
+  if ($keyword eq 'execute') {
+    sendResponse("OK\t0\t$trans\texecute done");
+    next;
+  }
+  if ($keyword eq 'get-control-point') {
+    my ($csp) = $mdl =~ m!:(.+)!;
+    if ($csp =~ m!LBCB!) {
+      sendResponse("OK   0       dummy   $mdl        x       displacement    5.036049E-1    y       displacement    1.557691E-4     z       displacement    -7.850649E-4    x       rotation        3.829964E-5     y       rotation       -2.747683E-5    z       rotation        1.195688E-3     x       force   6.296413E+0     y       force   1.451685E-1     z       force -1.252275E+0     x       moment  7.296673E-1     y       moment  -2.214440E+0    z       moment  3.522242E-2");
+    } else {
+      sendResponse("OK 0       dummy   $mdl              Ext.Long.LBCB2     external        2.422813E-1     Ext.Tranv.TopLBCB2  external        2.422813E-1     Ext.Tranv.Bot.LBCB2 external      2.422813E-1      Ext.Long.LBCB1       external        2.422813E-1     Ext.Tranv.LeftLBCB1 external        2.422813E-1     Ext.Tranv.RightLBCB1       external        2.422813E-1     ");
+    }
+    next;
+  }
+  if ($keyword eq 'propose') {
+    sendResponse("OK\t0\t$trans\tClose accepted");
+    $running = 0;
+    next;
+  }
 }
 
-sendCommand("close-session	dummy");
-receiveCommand();
-
 close $socket;
+close $lsocket;
 
-sub sendCommand {
+sub sendResponse {
 	my ($cmd) = @_;
 	print "Sending [$cmd]";
 	print $socket $cmd, "\n";
@@ -64,4 +74,10 @@ sub receiveCommand {
 	my $result = <$socket>;
 	chomp $result;
 	print "Received [$result]\n";
+        my ($keyword) = $result =~ m!([^\t]+)!;
+        my ($trans) = $result =~ m!(trans[^\t]+)!;
+        my ($mdl) = $result =~ m!(MDL-[^\t]+)!;
+        print "[$keyword|" . (defined $trans ? $trans : "undef") . "|" 
+          .(defined $mdl ? $mdl : "undef") . "]\n";
+        return ($keyword, $trans, $mdl);
 }
