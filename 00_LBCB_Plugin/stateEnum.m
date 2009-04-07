9bd0a9e8f32c0c6fff0d2683bc52f28c37bc676f
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-04-07 09:32:18 +0000

    Finished input file

---
+++ 00_LBCB_Plugin/stateEnum.m
@@ -0,0 +1,22 @@
+classdef  stateEnum < handle
+    properties
+    states = { };
+    idx = 1;
+    end
+    methods
+        function me = stateEnum(states)
+            me.states = states;
+            me.idx = 1;
+        end
+        function state = getState(me)
+            state = me.states{me.idx};
+        end
+        function setState(me,state)
+            tmp = find(strcmp(me.states,state));
+            me.idx = tmp(1);
+        end
+        function result = isState(state)
+            result = strcmp(getState(),state);
+        end
+    end
+end
