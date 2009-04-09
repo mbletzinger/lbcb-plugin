5ce4fac602f058048be7acca4f3e4ce2e99e8996
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-04-09 14:29:30 +0000

    Replaced spaces with tabs

--- branches/BeamColumn/tests/testMultiBoxNtcpLink.pl
+++ branches/BeamColumn/tests/testMultiBoxNtcpLink.pl
@@ -14,7 +14,6 @@ my $pwd = File::Spec->catdir(@dirs);
 
 
 my $lsocket = new IO::Socket::INET(
-                                   LocalAddr => $host,
                                    LocalPort => $port,
                                    Proto     => "tcp",
                                    Listen => 5,
@@ -25,7 +24,7 @@ die "Connection to $host:$port failed because $!" unless defined $lsocket;
 
 my $running = 1;
 my $socket = $lsocket->accept();
-
+print "Accepting connection from " . $socket->peerhost() . "\n"; 
 while ($running) {
   my($keyword, $trans, $mdl) = receiveCommand();
   if ($keyword eq 'open-session') {
@@ -47,9 +46,9 @@ while ($running) {
   if ($keyword eq 'get-control-point') {
     my ($csp) = $mdl =~ m!:(.+)!;
     if ($csp =~ m!LBCB!) {
-      sendResponse("OK   0       dummy   $mdl        x       displacement    5.036049E-1    y       displacement    1.557691E-4     z       displacement    -7.850649E-4    x       rotation        3.829964E-5     y       rotation       -2.747683E-5    z       rotation        1.195688E-3     x       force   6.296413E+0     y       force   1.451685E-1     z       force -1.252275E+0     x       moment  7.296673E-1     y       moment  -2.214440E+0    z       moment  3.522242E-2");
+      sendResponse("OK	0	dummy	$mdl	x	displacement	5.036049E-1	y	displacement	1.557691E-4	z	displacement	-7.850649E-4	x	rotation	3.829964E-5	y	rotation	-2.747683E-5	z	rotation	1.195688E-3	x	force	6.296413E+0	y	force	1.451685E-1	z	force	-1.252275E+0	x	moment	7.296673E-1	y	moment	-2.214440E+0	z	moment	3.522242E-2");
     } else {
-      sendResponse("OK 0       dummy   $mdl              Ext.Long.LBCB2     external        2.422813E-1     Ext.Tranv.TopLBCB2  external        2.422813E-1     Ext.Tranv.Bot.LBCB2 external      2.422813E-1      Ext.Long.LBCB1       external        2.422813E-1     Ext.Tranv.LeftLBCB1 external        2.422813E-1     Ext.Tranv.RightLBCB1       external        2.422813E-1     ");
+      sendResponse("OK	0	dummy	$mdl	Ext.Long.LBCB2	external	2.422813E-1	Ext.Tranv.TopLBCB2	external	2.422813E-1	Ext.Tranv.Bot.LBCB2	external	2.422813E-1	Ext.Long.LBCB1	external	2.422813E-1	Ext.Tranv.LeftLBCB1	external	2.422813E-1	Ext.Tranv.RightLBCB1	external	2.422813E-1");
     }
     next;
   }
@@ -58,6 +57,7 @@ while ($running) {
     $running = 0;
     next;
   }
+print " Could not understand [ $keyword	$trans	$mdl]\n";
 }
 
 close $socket;
