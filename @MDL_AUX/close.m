1bd7edd1caf1f31b076b4ea9091376073dab2062
sjkim4 <sjkim4@099a9394-a60e-4f42-8f05-0670266a236c>
2009-05-11 19:53:28 +0000

    close fuction in AUX module

--- @MDL_AUX/close.m
+++ @MDL_AUX/close.m
@@ -5,13 +5,10 @@ function close(obj,varargin)
 % Written by    7/21/2006 2:31AM O. Kwon
 % Last updated  7/21/2006 2:31AM O. Kwon
 % =====================================================================================================================
-if length(obj) > 0
-	LPLogger('Connecting remote sites',3,1);
-end
 
 for i=1:length(obj)
 	if varargin{1}(i)==1
-		try,
+		%try,
 	        LPLogger(sprintf('Disconnecting %s',obj(i).name),3,2);
 			if strcmp(obj(i).Comm_obj(i).Status,'open')                    % if connected
 				Sendvar_LabView(obj,sprintf('close-session\tdummy'));
@@ -19,10 +16,10 @@ for i=1:length(obj)
 			elseif strcmp(obj(i).Comm_obj(i).Status,'close')                % if already lost connection
 		    end
 		    LPLogger(sprintf('Disconnected\n'),3,3);
-		catch,
-	    	errstr = sprintf('Error closing connection.\nUnable to close remote site, %s', obj(i).name);
-	    	errmsg(errstr);
-		end
+		%catch,
+	    %	errstr = sprintf('Error closing connection.\nUnable to close remote site, %s', obj(i).name);
+	    %	disp(errstr);
+		%end
 	end
 end
 	
