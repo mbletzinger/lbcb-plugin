ba932e5681f4eb01e6406284608127d32234f1bb
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-05-31 12:04:21 +0000

    First set of commented classes

---
+++ DofData/DofData.m
@@ -0,0 +1,16 @@
+% =====================================================================================================================
+% Class containing degrees-of-freedom data.
+%
+% Members:
+%   disp - Translation and rotation DOFs Dx,Dy,Dz, Rx, Ry, Rz.
+%   force - Force and Moment DOFs Fx, Fy, Fz, Mx, My, Mz.
+%
+% Last updated  $DATE$ 
+% Author: $Author$
+% =====================================================================================================================
+classdef DofData < handle
+    properties
+        disp = zeros(6,1);   
+        force = zeros(6,1);    
+    end
+end
