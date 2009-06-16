f319e4e2dcf1e9fe38a9cdf1e473aded3372663b
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-06-16 10:51:47 +0000

    store handles in cell arrays

--- @LbcbPluginActions/LbcbPluginActions.m
+++ @LbcbPluginActions/LbcbPluginActions.m
@@ -1,13 +1,29 @@
 classdef LbcbPluginActions < handle
     properties
         handle = [];
+        commandLimitsHandles1 = {};
+        commandLimitsHandles2 = {};
+        commandTolerancesHandles1 = {};
+        commandTolerancesHandles2 = {};
+        incrementLimitsHandles1 = {};
+        incrementLimitsHandles2 = {};
+        
+        commandCurrentValueHandles1 = {};
+        commandCurrentValueHandles2 = {};
+        toleranceCurrentValueHandles1 = {};
+        toleranceCurrentValueHandles2 = {};
+        incrementCurrentValueHandles1 = {};
+        incrementCurrentValueHandles2 = {};
     end
     methods
         function me  = LbcbPlugin(handle)
             me.handle = handle;
         end
         initialize(me)
-        update(me)
+        updateCommandLimits(me)
+        updateIncrementLimits(me)
+        updateDofData(me)
+        updateStepTolerances(me)
         shutdown(me);
     end
 end
