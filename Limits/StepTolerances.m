9c531b88a4c9f84fa9bd9e419b45f693e1a9c5f4
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-06-27 15:36:45 +0000

    Limits don't handle one LBCB well

--- Limits/StepTolerances.m
+++ Limits/StepTolerances.m
@@ -2,6 +2,8 @@ classdef StepTolerances < handle
     properties
         within1 = zeros(12,1);
         within2 = zeros(12,1);
+        diffs1 = zeros(12,1);
+        diffs2 = zeros(12,1);
         limits = [];
     end
     methods
@@ -9,31 +11,24 @@ classdef StepTolerances < handle
             me.limits = LimitsDao('command.limits',cfg);
         end
         function yes = withinLimits(me,step)
-            yes = 1;
-            me.within1 = ones(12,1);
-            me.within2 = ones(12,1);
-            cmds1 = step.lbcb{1}.command;
-            rsp1 = step.lbcb{1}.response;
-            cmds2 = step.lbcb{2}.command;
-            rsp2 = step.lbcb{2}.response;
+            me.faults2 = ones(12,1);
+            [me.within1 me.diffs1 ] = me.wL(step.lbcb{1}.command,...
+                step.lbcb{1}.response,me.limits.window1,me.limits.used1);
+            lt = length(step.lbcb);
+            if lt > 1
+                [me.within2 me.diffs2 ] = me.wL(step.lbcb{2}.command,...
+                    step.lbcb{2}.response,me.limits.window2,me.limits.used2);
+            end
+            yes = (sum(me.faults1) + sum(me.faults2)) == 24;
+        end
+        function [within diff] = wL(me,command, response, window, used)
+            within = ones(12,1);
+            diff(1:6) = abs(command.disp - response.disp);
+            diff(7:12) = abs(command.force - response.force);
             for l = 1:12
-                if l > 6
-                    dof1 = abs(cmds1.force(l -6) - rsp1.force(l-6));
-                    dof2 = abs(cmds2.force(l -6) - rsp2.force(l-6));
-                else
-                    dof1 = abs(cmds1.disp(l) - rsp1.disp(l));
-                    dof2 = abs(cmds2.disp(l) - rsp2.disp(l));
-                end
                 if(me.limits.used1(l))
-                    if dof1 > me.limits.window1(l)
-                        yes = 0;
+                    if diff(l) > me.limits.window1(l)
                         me.within1(l) = 0;
-                    end
-                end
-                if(me.limits.used2(l))
-                    if dof2 > me.limits.window1(l)
-                        yes = 0;
-                        me.within2(l) = 0;
                     end
                 end
             end
