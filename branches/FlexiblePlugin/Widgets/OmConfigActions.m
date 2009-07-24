fe5750f14800cc5229c7e073d41157ed54459b57
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-07-24 23:46:39 +0000

    ED calc config

--- branches/FlexiblePlugin/Widgets/OmConfigActions.m
+++ branches/FlexiblePlugin/Widgets/OmConfigActions.m
@@ -4,7 +4,13 @@ classdef OmConfigActions < handle
         table = [];
     end
     methods
-        function me = OmConfigActions(me,cfg)
+        function me = OmConfigActions(handles, cfg)
+            ocfg = OmConfigDao(cfg);
+            me.table = { ocfg.sensorNames(:); ocfg.apply2Lbcb(:);ocfg.sensitivities(:);...
+                ocfg.baseX(:);ocfg.baseY(:);ocfg.baseZ(:);...
+                ocfg.platX(:);ocfg.platY(:);ocfg.platZ(:);ocfg.sensorErrorTol(:) }';
+            me.handles = handles;
+            
         end
         function setCell(me,row,column,str)
         end
