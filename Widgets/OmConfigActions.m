458261e6e5a3048436a28f99e32167213c91a149
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-07-27 18:00:58 +0000

    added perturbations

--- Widgets/OmConfigActions.m
+++ Widgets/OmConfigActions.m
@@ -5,6 +5,7 @@ classdef OmConfigActions < handle
         ocfg
         log = Logger
         aps
+        pertTable = cell(6,2);
     end
     methods
         function me = OmConfigActions(cfg)
@@ -13,6 +14,8 @@ classdef OmConfigActions < handle
             names = me.ocfg.sensorNames; 
             apply = me.ocfg.apply2Lbcb;
             sens = me.ocfg.sensitivities;
+            pert1 = me.ocfg.perturbationsL1;
+            pert2 = me.ocfg.perturbationsL2;
             bx = me.ocfg.baseX;
             by = me.ocfg.baseY;
             bz = me.ocfg.baseZ;
@@ -35,6 +38,14 @@ classdef OmConfigActions < handle
                 me.table{s,10} = err(s);
             end
             
+            for i = 1:6
+                if pert1.dispDofs(i)
+                    me.pertTable{i,1} = sprintf('%f',pert1.disp(1));
+                end
+                if pert2.dispDofs(i)
+                    me.pertTable{i,2} = sprintf('%f',pert2.disp(1));
+                end
+            end
         end
         function setCell(me,indices,data,errString)
             if isempty(data)
@@ -87,11 +98,12 @@ classdef OmConfigActions < handle
         function initialize(me,handles)
             me.handles = handles;
             format = {'char',{'LBCB1','LBCB2'},'numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric'};
+            set(me.handles.sensorTable,'ColumnFormat',format);
             set(me.handles.sensorTable,'Data',me.table);
              set(me.handles.numLbcbs,'String',{'1','2'});
             set(me.handles.numLbcbs,'Value',me.ocfg.numLbcbs);
-            set(me.handles.sensorTable,'ColumnFormat',format);
             set(me.handles.useFakeOm,'Value',me.ocfg.useFakeOm);
+            set(me.handles.pertTable,'Data',me.pertTable);
         
         end
         function setNumLbcbs(me,value)
@@ -99,6 +111,28 @@ classdef OmConfigActions < handle
         end
         function setUseFakeOm(me,value)
             me.ocfg.useFakeOm = value;
+        end
+        function setPertCell(me,indices,str)
+            if indices(2) == 1
+                pert = me.ocfg.perturbationsL1;
+            else
+                pert = me.ocfg.perturbationsL2;
+            end
+            if strcmp(str,'') || isempty(str)
+                pert.dispDofs(indices(1)) = 0;
+            else
+                data = sscanf(str,'%f');
+                if isempty(data)
+                    me.log.error(dbstack,sprintf('"%s" is not a valid input',str));
+                    return;
+                end
+                pert.setDispDof(indices(1),data);
+            end
+            if indices(2) == 1
+                me.ocfg.perturbationsL1 = pert;
+            else
+                me.ocfg.perturbationsL2 = pert;
+            end            
         end
     end
 end
