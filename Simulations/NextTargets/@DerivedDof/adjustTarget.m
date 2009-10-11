f921af596ea78da09a1465f187f0cfa239dc41ed
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-10-11 20:28:11 +0000

    Rearranging Directories

--- Simulations/NextTargets/@DerivedDof/adjustTarget.m
+++
@@ -1,5 +0,0 @@
-% generate a new LbcbStep based on the current step
-function adjustTarget(me,step)
-step.lbcbCps{1}.command.setForceDof(3,me.Fz1tar);
-step.lbcbCps{2}.command.setForceDof(3,me.Fz2tar);
-end
