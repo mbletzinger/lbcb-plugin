bb0d153a6161129948bbe175a998993961f29765
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-10-12 15:40:20 +0000

    Rearranging Directories

---
+++ branches/FlexiblePlugin/SimStates/SimulationSteps.m
@@ -0,0 +1,50 @@
+% =====================================================================================================================
+% Class containing the step count of the simulation.
+%
+% Members:
+%   step - Step number.
+%   subStep - Substep number.
+%   id - internal step count
+%   idCounter = static count of how many steps have been created
+%
+% $LastChangedDate$ 
+% $Author$
+% =====================================================================================================================
+classdef SimulationSteps < handle
+    properties
+        step = 0;
+        subStep = 0;
+        id = 0;
+        log = Logger;
+    end
+    methods
+        function me = SimulationSteps(step, subStep)
+            me.step = step;
+            me.subStep = subStep;
+            me.id = SimulationSteps.newId();
+            me.log.debug(dbstack,sprintf('created /step=%d/substep=%d/id=%d',me.step,me.subStep,me.id));
+        end
+        % increment the step or substep and return in a new instance
+        function simstate = nextStep(me,useSubStep)
+            stp = me.step;
+            sStp = me.subStep;
+            if(useSubStep)
+                sStp = sStp+ 1;
+            else
+                stp = stp + 1;
+                sStp = 0;
+            end
+            simstate = SimulationSteps(stp,sStp);
+        end
+        function str = toString(me)
+            str = sprintf('%d\t%d',me.step, me.subStep);
+        end
+    end
+    methods (Static, Access = private)
+        function id = newId()
+            global idCounter;
+            idCounter = idCounter + 1;
+            id = idCounter;
+        end
+    end
+end
