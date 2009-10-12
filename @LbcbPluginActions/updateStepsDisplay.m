2b8bbc6762b4247e19b50c0dde2e48afc9907b9e
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-10-12 15:37:28 +0000

    Rearranging Directories

--- @LbcbPluginActions/updateStepsDisplay.m
+++ @LbcbPluginActions/updateStepsDisplay.m
@@ -1,4 +1,4 @@
-function updateSteps(me)
+function updateStepsDisplay(me)
 simstep = me.nxtTgt.nextStepData.simstep;
 set(me.stepHandles{1},'String',sprintf('%d',simstep.step));
 set(me.stepHandles{2},'String',sprintf('%d',simstep.subStep));
