01631416dc5546a3f1a8ea0e33e8a0699bbdb9b4
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-10-15 11:44:15 +0000

    Simulation state refactoring

--- branches/FlexiblePlugin/@LbcbPluginActions/execute.m
+++ branches/FlexiblePlugin/@LbcbPluginActions/execute.m
@@ -1,3 +1,4 @@
+
 function execute(obj, event,me)
 % if me.running == 0  % in a hold state
 %     return;
@@ -31,75 +32,6 @@ switch a
 %            currentAction = me.currentAction
             me.currentAction.setState('READY');
             me.colorConnectionButton(me.ocOm.connectionType.getState());
-            stop(me.simTimer);
-        end
-    case'NEXT TARGET'
-        done = me.nxtTgt.isDone();
-        if done % Next target is ready
-            if me.nxtTgt.simCompleted  %  No more targets
-                me.setRunButton(0); % Pause the simulation
-                me.log.info(dbstack,'Simulation is Over');
-                me.currentAction.setState('READY');
-            else % Execute next step
-                if me.fakeOm == 0
-                    me.peOm.step = me.nxtTgt.nextStepData;
-                end
-                me.updateSteps();
-                me.currentAction.setState('CHECK LIMITS');
-            end
-        end
-    case 'OM PROPOSE EXECUTE'
-        if me.fakeOm == 0
-            done = me.peOm.isDone();
-            if done % execute response has been received from OM
-                if me.peOm.state.isState('ERRORS EXIST')
-                    me.setRunButton(0); % Pause the simulation
-                    me.ocOm.connectionStatus.setState('ERRORED');
-                    me.colorConnectionButton('OperationsManager');
-                    stop(me.simTimer);
-                end
-                me.gcpOm.step = me.peOm.step;
-                me.gcpOm.start();
-                me.currentAction.setState('OM GET CONTROL POINTS');
-            end
-        else
-            me.currentAction.setState('OM GET CONTROL POINTS');
-        end
-    case 'OM GET CONTROL POINTS'
-        if me.fakeOm
-            me.fakeGcp.generateControlPoints(me.nxtTgt.nextStepData);
-            me.nxtTgt.curStepData = me.nxtTgt.nextStepData;
-            me.currentAction.setState('NEXT TARGET');
-            me.arch.archive(me.nxtTgt.curStepData);
-            me.dd.update(me.nxtTgt.curStepData);
-        else
-            done = me.gcpOm.isDone();
-            if done
-                if me.peOm.state.isState('ERRORS EXIST')
-                    me.setRunButton(0); % Pause the simulation
-                    me.ocOm.connectionStatus.setState('ERRORED');
-                    me.colorConnectionButton('OperationsManager');
-                stop(me.simTimer);
-                end
-                me.nxtTgt.curStepData = me.gcpOm.step;
-                me.currentAction.setState('NEXT TARGET');
-                me.arch.archive(me.gcpOm.step);
-                me.dd.update(me.gcpOm.step);
-            end
-        end
-    case 'CHECK LIMITS'
-        %        me.nxtTgt
-        done = me.nxtTgt.withinLimits();
-        me.updateCommandLimits();
-        me.updateStepTolerances();
-        if done
-            me.currentAction.setState('OM PROPOSE EXECUTE');
-            if me.fakeOm == 0
-                me.peOm.step = me.nxtTgt.nextStepData;
-                me.peOm.start();
-            end
-        else
-            me.setRunButton(0); % Pause the simulation
             stop(me.simTimer);
         end
     case 'READY'
