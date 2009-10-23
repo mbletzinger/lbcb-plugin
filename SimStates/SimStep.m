e3a5d05038b28ecf120c4ab94db3ac664be335c4
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-10-23 20:58:42 +0000

    Added correction steps

--- SimStates/SimStep.m
+++ SimStates/SimStep.m
@@ -14,30 +14,40 @@ classdef SimStep < handle
     properties
         step = 0;
         subStep = 0;
+        correctionStep = 0;
         id = 0;
         log = Logger;
     end
     methods
-        function me = SimStep(step, subStep)
+        function me = SimStep(step, subStep, cStep)
             me.step = step;
             me.subStep = subStep;
+            me.correctionStep = cStep;
             me.id = SimStep.newId();
-            me.log.debug(dbstack,sprintf('created /step=%d/substep=%d/id=%d',me.step,me.subStep,me.id));
+            me.log.debug(dbstack,sprintf('created step %s',me.toString()));
         end
         % increment the step or substep and return in a new instance
-        function simstate = next(me,useSubStep)
+        function simstate = next(me,stepType)
             stp = me.step;
             sStp = me.subStep;
-            if(useSubStep)
-                sStp = sStp+ 1;
-            else
-                stp = stp + 1;
-                sStp = 0;
+            cStp = me.correctionStep;
+            switch stepType
+                case 0
+                    stp = stp + 1;
+                    sStp = 0;
+                    cStp = 0;
+                case 1
+                    sStp = sStp+ 1;
+                    cStp = 0;
+                case 2
+                    cStp = cStp+ 1;
+                otherwise
+                    me.log.error(dbstack, sprintf('%d not recognized',stepType));
             end
-            simstate = SimSteps(stp,sStp);
+            simstate = SimStep(stp,sStp,cStp);
         end
         function str = toString(me)
-            str = sprintf('%d\t%d',me.step, me.subStep);
+            str = sprintf('%d\t%d\t%d',me.step, me.subStep,me.correctionStep);
         end
     end
     methods (Static, Access = private)
