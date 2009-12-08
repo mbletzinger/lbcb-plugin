952b889c2a81870ad57d1b974f6b6c098ff4f217
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2009-12-08 03:50:35 +0000

    Fixes to limit test

--- Tests/LimitTest.m
+++ Tests/LimitTest.m
@@ -1,9 +1,14 @@
 
-cfg = Configuration;
-cfg.load;
 loops = {'INCREMENT' 'UPPER' 'LOWER' 'STEP'};
 for l = 1:length(loops)
-    s = SetupLimitTest(cfg);
+    cfg = Configuration;
+    s = SetupTest();
+    s.cfg = cfg;
+    s.genOmConfig();
+    s.genNetworkConfig();
+    s.genLogConfig()
+    hfact = HandleFactory([],cfg);
+    s.hfact = hfact;
     s.setTest(loops{l});
     LbcbPlugin('hfact',s.hfact,'notimer',0);
     delete(s);
