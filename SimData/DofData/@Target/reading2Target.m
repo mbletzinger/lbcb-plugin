4638db69dd4621787a1d05ff7367f293dc67c5ac
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2010-01-16 03:51:48 +0000

    Switch to sending all responses to simcor

--- SimData/DofData/@Target/reading2Target.m
+++ SimData/DofData/@Target/reading2Target.m
@@ -1,6 +1,7 @@
 % Imports the LBCB reading
 function reading2Target(me,reading)
 me.disp = reading.disp;
+me.dispDofs = ones(6,1);
 me.force = reading.force;
-me.clearNonControlDofs();
+me.forceDofs = ones(6,1);
 end
