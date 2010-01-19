5efa9b329d9f719ee2a287ec8cb5a860dd3c93e3
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2010-01-19 01:13:28 +0000

    Three Pier rubber specimen testing

--- UserFunctions/ControlPointTransformation/ThreePierTransformCommand.m
+++ UserFunctions/ControlPointTransformation/ThreePierTransformCommand.m
@@ -6,18 +6,18 @@ lbcbTgts = { Target Target }; % create one Target for each LBCB
 
 % LBCB 1
 
-% Set Dx on LBCB 1
-lbcbTgts{1}.setDispDof(1,-mdlTgts{2}.disp(1));
-% Set Dy on LBCB 1
+% Set Dx on LBCB 2
+lbcbTgts{1}.setDispDof(1,mdlTgts{2}.disp(1));
+% Set Dy on LBCB 2
 %lbcbTgts{1}.setDispDof(2,mdlTgts{2}.disp(3));
-% Set Dz on LBCB 1
+% Set Dz on LBCB 2
 lbcbTgts{1}.setDispDof(3,-mdlTgts{2}.disp(2));
-% Set Rx on LBCB 1
+% Set Rx on LBCB 2
 %lbcbTgts{1}.setDispDof(4,mdlTgts{2}.disp(4));
-% Set Ry on LBCB 1
-lbcbTgts{1}.setDispDof(5,-mdlTgts{2}.disp(6));
-% Set Rz on LBCB 1
-%lbcbTgts{1}.setDispDof(6,mdlTgts{2}.disp(5));
+% Set Ry on LBCB 2
+lbcbTgts{1}.setDispDof(5,mdlTgts{2}.disp(6));
+% Set Rz on LBCB 2
+%lbcbTgts{1}.setDispDof(6,-mdlTgts{2}.disp(5));
 
 
 % LBCB 2
