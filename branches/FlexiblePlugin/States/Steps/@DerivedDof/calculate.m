44fd001764d98581e1e65722f69652104cf0e38b
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2010-07-14 20:54:01 +0000

    Corrections refactoring

---
+++ branches/FlexiblePlugin/States/Steps/@DerivedDof/calculate.m
@@ -0,0 +1,11 @@
+% generate a new LbcbStep based on the current step
+function calculate(me,cstep)
+me.loadCfg();
+scfg = StepCorrectionConfigDao(me.cdp.cfg);
+funcs = scfg.ddCalculationFunctions;
+if strcmp(funcs{2 + level},'<NONE>')
+    return;
+end
+ddCalc = str2func(funcs{2 + level});
+ddCalc(me,cstep);
+end
