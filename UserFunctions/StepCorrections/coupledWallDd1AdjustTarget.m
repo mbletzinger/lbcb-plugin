31b6d711e576e20c3062ac61cf8e7f52571a2c1a
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2010-12-22 19:53:50 +0000

    User functions by Ken for testing in Dec. 2010

--- UserFunctions/StepCorrections/coupledWallDd1AdjustTarget.m
+++ UserFunctions/StepCorrections/coupledWallDd1AdjustTarget.m
@@ -1,14 +1,27 @@
-function coupledWallDd1AdjustTarget(me,step)
+function coupledWallDd1AdjustTarget(me,step,tcps)
+ddx = me.getArch('ddx');    % Load dx increment
 
-command_xL = step.lbcbCps{1}.command.disp(1);
-command_xR = step.lbcbCps{2}.command.disp(1);
+% Adjust OM dx command displacements by ddx
+dx1Target = step.lbcbCps{1}.command.disp(1) - ddx;
+dx2Target = step.lbcbCps{2}.command.disp(1) + ddx;
+step.lbcbCps{1}.command.setDispDof(1,dx1Target);
+step.lbcbCps{2}.command.setDispDof(1,dx2Target);
 
-ddx = me.getArch('ddx');
+% Adjust OM ry command displacements through dx2ry
+dx2ryBase = me.getCfg('dx2ryBase');
+dx2ryRedRate = me.getCfg('dx2ryRedRate');
+maxDisp = me.getArch('maxDisp');
+dx2ry = dx2ryRedRate*maxDisp + dx2ryBase;
 
-command_xL = command_xL - ddx;
-command_xR = command_xR + ddx;
+ry1Target = step.lbcbCps{1}.command.disp(5) - ddx*dx2ry;
+ry2Target = step.lbcbCps{2}.command.disp(5) + ddx*dx2ry;
+step.lbcbCps{1}.command.setDispDof(5,ry1Target);
+step.lbcbCps{2}.command.setDispDof(5,ry2Target);
 
-step.lbcbCps{1}.command.setDispDof(1,command_xL);
-step.lbcbCps{2}.command.setDispDof(1,command_xR);
-
+% Update target displacements
+tcps.lbcbCps{1}.command.disp(1) = tcps.lbcbCps{1}.command.disp(1) - ddx;
+tcps.lbcbCps{2}.command.disp(1) = tcps.lbcbCps{2}.command.disp(1) + ddx;
+tcps.lbcbCps{1}.command.disp(5) = tcps.lbcbCps{1}.command.disp(1)*dx2ry;
+tcps.lbcbCps{2}.command.disp(5) = tcps.lbcbCps{2}.command.disp(1)*dx2ry;
+me.putDat('dd1Flag',1);
 end
--- UserFunctions/StepCorrections/coupledWallEdPrelimAdjust.m
+++ UserFunctions/StepCorrections/coupledWallEdPrelimAdjust.m
@@ -15,7 +15,17 @@ newOMcommandR = prevOMcommandR + (targetR - positionR);
 nextStep.lbcbCps{1}.command.disp([1 5]) = newOMcommandL([1 5]);
 nextStep.lbcbCps{2}.command.disp([1 5]) = newOMcommandR([1 5]);
 
-nextStep.lbcbCps{1}.command.setForceDof(3,me.getDat('Fz1Target'));
-nextStep.lbcbCps{2}.command.setForceDof(3,me.getDat('Fz2Target'));
+if me.getCfg('PrelimEDFzFlag') == 1
+    nextStep.lbcbCps{1}.command.setForceDof(3,me.getDat('Fz1Target'));
+    nextStep.lbcbCps{2}.command.setForceDof(3,me.getDat('Fz2Target'));
+    nextStep.lbcbCps{1}.command.setForceDof(5,me.getDat('My1Target'));
+    nextStep.lbcbCps{2}.command.setForceDof(5,me.getDat('My2Target'));    
+elseif me.getCfg('PrelimEDFzFlag') == 2
+    nextStep.lbcbCps{1}.command.setForceDof(3,curStep.lbcbCps{1}.response.force(3));
+    nextStep.lbcbCps{2}.command.setForceDof(3,curStep.lbcbCps{2}.response.force(3));
+    nextStep.lbcbCps{1}.command.setForceDof(5,curStep.lbcbCps{1}.response.force(5));
+    nextStep.lbcbCps{2}.command.setForceDof(5,curStep.lbcbCps{2}.response.force(5));
+end
+
 
 end
