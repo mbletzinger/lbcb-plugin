3710a3d5931793b292fb8aedeaa5c35c3e0afccc
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-02-08 15:20:31 +0000

    New Plot

---
+++ DataDisplay/DofLabels.m
@@ -0,0 +1,24 @@
+classdef DofLabels < handle
+    properties
+        labels
+    end
+    methods
+        function me = DofLabels()
+            me.labels = org.nees.uiuc.simcor.matlab.HashTable();
+            list = { 'Dx', 'Dy', 'Dz','Rx', 'Ry', 'Rz',...
+                'Fx', 'Fy', 'Fz','Mx', 'My', 'Mz'};
+            for l = 1:length(list)
+                me.put(list{l},l);
+            end
+        end
+        function val = get(me,lbl)
+            val = me.labels.get(lbl);
+        end
+        function put(me,lbl,val)
+            me.labels.put(lbl,val);
+        end
+        function yes = existsDat(me,key)
+            yes = me.labels.exists(key);
+        end
+    end
+end
