7255fcc6501586646276e121018b59add1f13d2e
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-03-15 14:13:57 +0000

    Vamp now includes current step number.

--- branches/WharfPier/@LbcbPluginActions/vampCheck.m
+++ branches/WharfPier/@LbcbPluginActions/vampCheck.m
@@ -1,6 +1,6 @@
 
 function vampCheck(obj, event,me) %#ok<INUSL>
-if rem(me.vampTimerCnt,100) == 0
+if rem(me.vampTimerCnt,500) == 0
     me.log.debug(dbstack,'Vamp Check Executing')
 end
 me.vampTimerCnt = me.vampTimerCnt + 1;
