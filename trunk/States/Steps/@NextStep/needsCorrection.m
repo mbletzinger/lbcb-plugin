26284122e0517a0879f8be5a0c0ac0b398927b80
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-05-25 03:22:19 +0000

    Refactoring Corrections code

--- trunk/States/Steps/@NextStep/needsCorrection.m
+++ trunk/States/Steps/@NextStep/needsCorrection.m
@@ -28,34 +28,7 @@ end
 
 me.checkedStepNumber = stepN;
 
-for lv = 1:length(doCorrections)
-    if doCorrections(lv) == true;
-        switch lv
-            case 1
-                n1 = me.ed{1}.needsCorrection(me.dat.curStepData.lbcbCps{1},...
-                    me.dat.correctionTarget.lbcbCps{1});
-                n2 = 0;
-                if me.cdp.numLbcbs() == 2
-                    n2 = me.ed{2}.needsCorrection(me.dat.curStepData.lbcbCps{2},...
-                        me.dat.correctionTarget.lbcbCps{2});
-                end
-                me.edCorrect = (n1 + n2) > 0;
-                needsCorrection = me.edCorrect;
-            case { 2 3 4 5 }
-                ddCorrect = me.dd{lv-1}.needsCorrection();
-                if ddCorrect
-                    me.ddlevel = lv-1;
-                    needsCorrection = ddCorrect;
-                    me.needsCorrectionFlag = needsCorrection;
-                    return;
-                end
-                needsCorrection = me.edCorrect;
-                if lv == 2 && me.edCorrect % Only do first level DD with ED
-                    me.needsCorrectionFlag = needsCorrection;
-                    return;
-                end
-        end
-    end
+if lv == 2 && me.edCorrect % Only do first level DD with ED
     me.needsCorrectionFlag = needsCorrection;
-    
+    return;
 end
