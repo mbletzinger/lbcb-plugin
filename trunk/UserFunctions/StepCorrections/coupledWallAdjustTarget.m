95eeaf8ceb3356f16ac6356ffff10e993dd5674c
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-08-16 05:28:11 +0000

    Removed old test files

--- trunk/UserFunctions/StepCorrections/coupledWallAdjustTarget.m
+++
@@ -1,7 +0,0 @@
-% generate a new LbcbStep based on the current step
-function coupledWallAdjustTarget(me,step)
-Fz1tar = me.getDat('Fz1tar');
-Fz2tar = me.getDat('Fz2tar');
-step.lbcbCps{1}.command.setForceDof(3,Fz1tar);
-step.lbcbCps{2}.command.setForceDof(3,Fz2tar);
-end
--- trunk/UserFunctions/StepCorrections/coupledWallDd0AdjustTargetNew.m
+++
@@ -1,7 +0,0 @@
-% generate a new LbcbStep based on the current step
-function coupledWallDd0AdjustTargetNew(me,step,tcps)
-step.lbcbCps{1}.command.setForceDof(3,me.getDat('Fz1Target'));
-step.lbcbCps{2}.command.setForceDof(3,me.getDat('Fz2Target'));
-step.lbcbCps{1}.command.setForceDof(5,me.getDat('My1Target'));
-step.lbcbCps{2}.command.setForceDof(5,me.getDat('My1Target'));
-end
