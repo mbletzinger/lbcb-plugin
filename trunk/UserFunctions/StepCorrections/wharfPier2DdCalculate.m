4feb70345a821921e1f0012f55033870aeb2c454
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-08-26 01:55:34 +0000

    Wharf Pier Test 2 fixes

--- trunk/UserFunctions/StepCorrections/wharfPier2DdCalculate.m
+++ trunk/UserFunctions/StepCorrections/wharfPier2DdCalculate.m
@@ -8,16 +8,19 @@ if cstep.isInitialPosition
     me.putDat('initialPosition',cstep.lbcbCps{1}.response.disp(1));
 end
 pierHeight = me.getCfg('PierHeight');
-infPFactor = me.getCfg('InfPFactor');
+crackPosition = me.getCfg('CrackPosition');
+crackSlipTTerm = me.getCfg('CrackSlipTTerm');
+crackSlipBTerm = me.getCfg('CrackSlipBTerm');
 ip = me.getDat('initialPosition');
 P_Delta = fz * (dx - ip);
 Shear_L = fx * pierHeight;
 MomentY = -my;
-
+MCrack = MomentY + crackPosition * fx + fz * dx * crackSlipTTerm/crackSlipBTerm;
 myBottom = MomentY + Shear_L + P_Delta;
-Inflec_Point = infPFactor-myBottom/fx;
+Inflec_Point = pierHeight-myBottom/fx;
 me.putArch('MyBottom',myBottom);
 me.putArch('MomentY', MomentY);
+me.putArch('MCrack', MCrack);
 me.putArch('InflecPoint',Inflec_Point);
 me.putArch('PDelta', P_Delta);
 me.putArch('ShearL', Shear_L);
