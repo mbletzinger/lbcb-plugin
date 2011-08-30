e0395130e0322f8e7be43503952a6272df044fc4
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-08-30 00:26:36 +0000

    Fixed Prelim adjust

--- branches/WharfPierTest2/States/Steps/@ElasticDeformation/prelimAdjust.m
+++ branches/WharfPierTest2/States/Steps/@ElasticDeformation/prelimAdjust.m
@@ -10,12 +10,12 @@ end
 target = nextStep.lbcbCps{lbcb}.command.disp;
 prevOMcommand = curStep.lbcbCps{lbcb}.command.disp;
 position = curStep.lbcbCps{lbcb}.response.ed.disp;
-correction = target - prevOMcommand;
-newOMcommand = position + correction;
-me.log.debug(dbstack,sprintf('Orig Target: %s',nextStep.lbcbCps{lbcb}.command.toString()));
-nextStep.lbcbCps{lbcb}.command.disp = newOMcommand;
-me.log.debug(dbstack,sprintf('New Target: %s',nextStep.lbcbCps{lbcb}.command.toString()));
-me.log.debug(dbstack,sprintf('Prev Cmd: %s',curStep.lbcbCps{lbcb}.command.toString()));
+correction = prevOMcommand - position;
+newOMcommand = target + correction;
 me.log.debug(dbstack,sprintf('Prev Position: %s',curStep.lbcbCps{lbcb}.response.toString()));
+me.log.debug(dbstack,sprintf('Prev Cmd: %s',curStep.lbcbCps{lbcb}.command.toString()));
+me.log.debug(dbstack,sprintf('New Target: %s',nextStep.lbcbCps{lbcb}.command.toString()));
+nextStep.lbcbCps{lbcb}.command.disp = newOMcommand;
+me.log.debug(dbstack,sprintf('New Cmd: %s',nextStep.lbcbCps{lbcb}.command.toString()));
 %me.archiveCorrections('prelimEd',correction);
 end
