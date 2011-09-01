a2a49b4952187d4c839ba868ec4115675d611bcc
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-09-01 17:33:15 +0000

    Wharf Pier Final test

--- branches/WharfPierTest2/@LbcbPluginResults/updateStepTolerances.m
+++ branches/WharfPierTest2/@LbcbPluginResults/updateStepTolerances.m
@@ -1,5 +1,8 @@
 function updateStepTolerances(me)
 stp = me.hfact.dat.curStepData;
+if isempty(stp)
+    return;
+end
 me.log.debug(dbstack,sprintf('Updating tolerances for step %s',stp.stepNum.toStringD(' ')));
 me.tolerances.setStep(stp);
 me.tolerances.setTarget(me.hfact.dat.correctionTarget);
