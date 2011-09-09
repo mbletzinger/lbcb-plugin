f935589ed279b22b602818d51c97141afcf105a4
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2011-09-09 22:20:28 +0000

    Chia-Ming fixes to accomodate tolerance window

--- branches/CaberSmallPierTest/States/Steps/@ElasticDeformation/prelimAdjust.m
+++ branches/CaberSmallPierTest/States/Steps/@ElasticDeformation/prelimAdjust.m
@@ -7,10 +7,17 @@ end
 %    me.log.info(dbstack,'Skipping prelim ED adjust on first step');
 %    return;
 %end
+%------------------
+% added by Chia-Ming
+%------------------
+ind = find(me.st.used(1:6)==1);
+% position = zeros(6,1);
+%==
 target = nextStep.lbcbCps{lbcb}.command.disp;
 prevOMcommand = curStep.lbcbCps{lbcb}.command.disp;
 position = curStep.lbcbCps{lbcb}.response.ed.disp;
-correction = prevOMcommand - position;
+correction = zeros(6,1);
+correction(ind) = prevOMcommand(ind) - position(ind);
 newOMcommand = target + correction;
 me.log.debug(dbstack,sprintf('Prev Position: %s',curStep.lbcbCps{lbcb}.response.toString()));
 me.log.debug(dbstack,sprintf('Prev Cmd: %s',curStep.lbcbCps{lbcb}.command.toString()));
