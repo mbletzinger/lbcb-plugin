0b45490528bbd9c6f6c47868653d2c7a6b1e3aa7
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2013-02-23 22:01:18 +0000

    Added time widget

---
+++ SimData/Time/TimeCalculator.m
@@ -0,0 +1,43 @@
+classdef TimeCalculator < handle
+    properties
+        start
+        millis
+        running
+        paused
+        ptime
+        pinterval
+    end
+    methods
+        function me = TimeCalculator()
+            me.running = false;
+            me.paused = false;
+            me.millis = 0;
+            me.pinterval = 0;
+        end
+        function time2Pause(me,p)
+            me.paused = p;
+            if(p)
+                me.ptime = cputime;
+            else
+                me.pinterval = me.pinterval + cputime - me.ptime;
+            end
+        end
+        function time2Start(me)
+            me.start=cputime;
+            me.running = true;
+        end
+        function tm = time2Stop(me)
+            me.running = false;
+            me.millis = cputime - me.start - me.pinterval;
+            tm = me.get();
+        end
+        function tm = get(me)
+            if(me.running)
+                millis = cputime - me.start - me.pinterval; %#ok<*PROP>
+            else
+                millis = me.millis;
+            end
+            tm = TimeRep(millis);
+        end
+    end
+end
