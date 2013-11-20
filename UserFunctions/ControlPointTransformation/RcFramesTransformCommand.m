2c6b18ef070f0a5c5a9777fdb781abc26a7457a4
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2013-11-20 19:13:32 +0000

    RC Frames and added LBCB cartesian readings to the initial position code

---
+++ UserFunctions/ControlPointTransformation/RcFramesTransformCommand.m
@@ -0,0 +1,44 @@
+function lbcbTgts = RcFramesTransformCommand(me,mdlTgts)  
+numLbcbs = me.cdp.numLbcbs();
+lbcbTgts = { Target };
+if numLbcbs > 1
+    lbcbTgts = { lbcbTgts{1}, Target};
+end
+
+%Coordinate Transformation from LBCB to Model
+%Model Coordinates
+% Model Dx = LBCB Dy
+% Model Dy = LBCB Dx
+% Model Dz = LBCB -Dz
+
+for lbcb = 1:numLbcbs
+    me.setDat(sprintf('L%d.Cmd.Dx',lbcb), mdlTgts{lbcb}.disp(1));
+    me.setDat(sprintf('L%d.Cmd.Dy',lbcb), mdlTgts{lbcb}.disp(2));
+    me.setDat(sprintf('L%d.Cmd.Dz',lbcb), mdlTgts{lbcb}.disp(3));
+    me.setDat(sprintf('L%d.Cmd.Rx',lbcb), mdlTgts{lbcb}.disp(4));
+    me.setDat(sprintf('L%d.Cmd.Ry',lbcb), mdlTgts{lbcb}.disp(5));
+    me.setDat(sprintf('L%d.Cmd.Rz',lbcb), mdlTgts{lbcb}.disp(6));
+    % Set Dx
+    lbcbTgts{lbcb}.setDispDof(1,mdlTgts{lbcb}.disp(2));
+    % Set Dy
+    lbcbTgts{lbcb}.setDispDof(2,mdlTgts{lbcb}.disp(1));
+    % Set Dz
+    lbcbTgts{lbcb}.setDispDof(3,-mdlTgts{lbcb}.disp(3));
+    % Set Rx
+    lbcbTgts{lbcb}.setDispDof(4,mdlTgts{lbcb}.disp(5));
+    % Set Ry
+    lbcbTgts{lbcb}.setDispDof(5,mdlTgts{lbcb}.disp(4));
+    % Set Rz
+    lbcbTgts{lbcb}.setDispDof(6,-mdlTgts{lbcb}.disp(6));
+    % me.log.debug(dbstack, sprintf('M2 and L1 %s and %s', mdlTgts{2}.toString(),lbcbTgts{lbcb}.toString()));
+end
+% scale factor=[disp,rot,force,moment]
+scale_factor = zeros(4,1);
+scale_factor(1) = me.getCfg('Displacement.Scale');
+scale_factor(2) = me.getCfg('Rotation.Scale');
+scale_factor(3) = me.getCfg('Force.Scale');
+scale_factor(4) = me.getCfg('Moment.Scale');
+
+ [lbcbTgts{1}.disp,lbcbTgts{2}.disp] = scale_command(scale_factor,lbcbTgts);
+
+end
---
+++ UserFunctions/ControlPointTransformation/RcFramesTransformResponse.m
@@ -0,0 +1,82 @@
+function mdlTgts = RcFramesTransformResponse(me,lbcbTgts)
+numLbcbs = me.cdp.numLbcbs();
+mdlTgts = { Target };
+if numLbcbs > 1
+    mdlTgts = { mdlTgts{1}, Target};
+end
+
+%Coordinate Transformation from LBCB to Model
+%Model Coordinates
+% Model Dx = LBCB Dy
+% Model Dy = LBCB Dx
+% Model Dz = LBCB -Dz
+numLbcbs = me.cdp.numLbcbs();
+if me.existsCfg('FakeResponse')
+    fake = me.getCfg('FakeResponse');
+else
+    fake = 0;
+end
+stiffnesses = zeros(6,1);
+if fake
+    stiffnesses(1) = me.getCfg('Stiffness.Dx');
+    stiffnesses(2) = me.getCfg('Stiffness.Dy');
+    stiffnesses(3) = me.getCfg('Stiffness.Dz');
+    stiffnesses(4) = me.getCfg('Stiffness.Rx');
+    stiffnesses(5) = me.getCfg('Stiffness.Ry');
+    stiffnesses(6) = me.getCfg('Stiffness.Rz');
+end
+
+for lbcb = 1:numLbcbs
+    
+    % Set Dx
+    mdlTgts{lbcb}.setDispDof(1,lbcbTgts{lbcb}.disp(2));
+    % Set Dy
+    mdlTgts{lbcb}.setDispDof(2,lbcbTgts{lbcb}.disp(1));
+    % Set Dz
+    mdlTgts{lbcb}.setDispDof(3,-lbcbTgts{lbcb}.disp(3));
+    % Set Rx
+    mdlTgts{lbcb}.setDispDof(4,lbcbTgts{lbcb}.disp(5));
+    % Set Ry
+    mdlTgts{lbcb}.setDispDof(5,lbcbTgts{lbcb}.disp(4));
+    % Set Rz
+    mdlTgts{lbcb}.setDispDof(6,-lbcbTgts{lbcb}.disp(6));
+    
+    if fake
+        cmd(1) = me.getDat(sprintf('L%d.Cmd.Dx',lbcb));
+        cmd(2) = me.getDat(sprintf('L%d.Cmd.Dy',lbcb));
+        cmd(3) = me.getDat(sprintf('L%d.Cmd.Dz',lbcb));
+        cmd(4) = me.getDat(sprintf('L%d.Cmd.Rx',lbcb));
+        cmd(5) = me.getDat(sprintf('L%d.Cmd.Ry',lbcb));
+        cmd(6) = me.getDat(sprintf('L%d.Cmd.Rz',lbcb));
+        forces = cmd * stiffnesses;
+    else
+        forces = lbcbTgts{lbcb}.force;
+    end
+    
+    % Set Dx from LBCB 2
+    mdlTgts{lbcb}.setForceDof(1,forces(1));
+    % Set Dy from LBCB 2
+    mdlTgts{lbcb}.setForceDof(2,-forces(3));
+    % Set Dz from LBCB 2
+    mdlTgts{lbcb}.setForceDof(3,forces(2));
+    % Set Rx from LBCB 2
+    mdlTgts{lbcb}.setForceDof(4,forces(4));
+    % Set Ry from LBCB 2
+    mdlTgts{lbcb}.setForceDof(5,-forces(6));
+    % Set Rz from LBCB 2
+    mdlTgts{lbcb}.setForceDof(6,forces(5));
+end
+
+
+if me.existsCfg('Displacement.Scale')
+    % scale factor=[disp,rot,force,moment]
+    scale_factor = zeros(4,1);
+    scale_factor(1) = me.getCfg('Displacement.Scale');
+    scale_factor(2) = me.getCfg('Rotation.Scale');
+    scale_factor(3) = me.getCfg('Force.Scale');
+    scale_factor(4) = me.getCfg('Moment.Scale');
+    [mdlTgts{1}.disp,mdlTgts{2}.disp,mdlTgts{1}.force,mdlTgts{2}.force] = scale_command_response(scale_factor,mdlTgts);
+end
+
+
+end
