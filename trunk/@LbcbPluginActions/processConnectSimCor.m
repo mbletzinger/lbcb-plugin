5aa21b5c4c96a0629d71c82b2a3f492975d9011e
mbletzin <mbletzin@099a9394-a60e-4f42-8f05-0670266a236c>
2014-07-01 14:40:38 +0000

    More fixes to the SimCor shutdown dialog button.

--- trunk/@LbcbPluginActions/processConnectSimCor.m
+++ trunk/@LbcbPluginActions/processConnectSimCor.m
@@ -1,6 +1,7 @@
-function processConnectSimCor(me,on)
+function cancel = processConnectSimCor(me,on)
 action = 'CLOSE SIMCOR CONNECTION';
-
+nope = [];
+cancel = 0;
 if on
     action = 'OPEN SIMCOR CONNECTION';
     nope = me.hfact.ocSimCor.start(0);
@@ -8,6 +9,8 @@ else
     button = questdlg('Do you want to end the simulation?','Stopping Simulation','Proceed','Cancel','Cancel');
     if strcmp(button,'Proceed')
         nope = me.hfact.ocSimCor.start(1);
+    else
+        cancel = 1;
     end
 end
 if nope
